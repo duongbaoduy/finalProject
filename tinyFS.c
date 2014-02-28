1b6d63ce3e1c50a4149b2081fe2eb9a0f3a60b09
sccalabr <sccalabr@calpoly.edu>
2014-02-27 19:57:18 -0800

    Update tinyFS.c

--- tinyFS.c
+++ tinyFS.c
@@ -8,15 +8,21 @@ int tfs_mount(char *filename);
 
 int tfs_unmount(void);
 
-/* Opens a file for reading and writing on the currently mounted file system. Creates a dynamic resource table entry for the file, and returns a file descriptor (integer) that can be used to reference this file while the filesystem is mounted. */
+/* Opens a file for reading and writing on the currently mounted file system. 
+Creates a dynamic resource table entry for the file, and returns a file descriptor 
+(integer) that can be used to reference this file while the filesystem is mounted. 
+*/
 
 fileDescriptor tfs_openFile(char *name);
 
-/* Closes the file, de-allocates all system/disk resources, and removes table entry */
+/* Closes the file, de-allocates all system/disk resources, 
+and removes table entry */
 
 int tfs_closeFile(fileDescriptor FD);
 
-/* Writes buffer ‘buffer’ of size ‘size’, which represents an entire file’s content, to the file system. Sets the file pointer to 0 (the start of file) when done. Returns success/error codes. */
+/* Writes buffer ‘buffer’ of size ‘size’, which represents an entire file’s 
+content, to the file system. Sets the file pointer to 0 (the start of file) 
+when done. Returns success/error codes. */
 
 int tfs_writeFile(fileDescriptor FD,char *buffer, int size);
 
@@ -24,11 +30,15 @@ int tfs_writeFile(fileDescriptor FD,char *buffer, int size);
 
 int tfs_deleteFile(fileDescriptor FD);
 
-/* reads one byte from the file and copies it to buffer, using the current file pointer location and incrementing it by one upon success. If the file pointer is already at the end of the file then tfs_readByte() should return an error and not increment the file pointer. */
+/* reads one byte from the file and copies it to buffer, using the current file 
+pointer location and incrementing it by one upon success. If the file pointer is 
+already at the end of the file then tfs_readByte() should return an error and not 
+increment the file pointer. */
 
 int tfs_readByte(fileDescriptor FD, char *buffer);
 
-/* change the file pointer location to offset (absolute). Returns success/error codes.*/
+/* change the file pointer location to offset (absolute). 
+Returns success/error codes.*/
 
 int tfs_seek(fileDescriptor FD, int offset);
 
@@ -38,7 +48,8 @@ In your libTinyFS.h file, you must include the following definitions:
 
 #define BLOCKSIZE 256
 
-/* Your program should use a 10240 Byte disk size giving you 40 blocks total. This is a default size. You must be able to support different possible values */
+/* Your program should use a 10240 Byte disk size giving you 40 blocks total. 
+This is a default size. You must be able to support different possible values */
 
 #define DEFAULT_DISK_SIZE 10240
 
