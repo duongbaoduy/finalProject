1f5fbeffadf21aa2a8f456e3ba05f69cb99cd381
sccalabr <sccalabr@calpoly.edu>
2014-03-02 23:32:55 -0800

    Update tinyFS.c

--- tinyFS.c
+++ tinyFS.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tinyFS.h"
+#include "libDisk.h"
+#include <string.h>
+
 #define TYPE_OFFSET 0
 #define MAGIC_NUMBER 0x45
 #define MAGIC_NUMBER_OFFSET 1
@@ -14,7 +20,54 @@
 #define INODE_BLOCK_NUMBER_OFFSET 3
 #define NEXT_BLOCK_NUMBER_OFFSET 4
 #define TEXT_OFFSET  5
+//TYPES
+#define SUPERBLOCK_TYPE 1
+#define INODE_TYPE 2
+#define FILE_EXTENT_TYPE 3
+#define FREE_TYPE 4
 
+SuperBlock *superBlock;
+INode *rootINode;
+fileDescriptor fd;
+
+INode *makeInode(unsigned char blockNum, char *filename, unsigned char data) {
+   RequiredInfo requiredInfo;
+   INode *iNode = calloc(sizeof(INode), 1);
+   
+   requiredInfo.type = 2;
+   requiredInfo.magicNumber = 0x45;
+   requiredInfo.blockNumber = blockNum;
+   
+   rootINode->required = requiredInfo;
+   rootINode->filename = filename;
+   rootINode->size = 15;
+   rootINode->data = data;
+   rootINode->iNodeList = NULL;
+   
+   return iNode;
+}
+FreeBlock *makeFreeBlock(int blockNum) {
+   RequiredInfo requiredInfo;
+   INode *iNode = calloc(sizeof(INode), 1);
+   
+   requiredInfo.type = 4;
+   requiredInfo.magicNumber = 0x45;
+   requiredInfo.blockNumber = blockNum;
+   
+   FreeBlock *freeBlock = calloc(sizeof(FreeBlock), 1);
+   
+   freeBlock->required = requiredInfo;
+   
+   return freeBlock;
+}
+
+void cleanBlock(int blockNum) {
+   char *buffer = calloc(BLOCKSIZE, 1);
+   memset(buffer + TYPE_OFFSET, FREE_TYPE, 1);
+   memset(buffer + MAGIC_NUMBER_OFFSET, MAGIC_NUMBER, 1);
+   memset(buffer + BLOCK_NUMBER_OFFSET, blockNum, 1);
+   writeBlock(fd, blockNum, buffer);
+}
 
 /* Makes a blank TinyFS file system of size nBytes on the file specified by
 ‘filename’. This function should use the emulated disk library to open the
@@ -22,7 +75,45 @@ specified file, and upon success, format the file to be mountable. This includes
 initializing all data to 0x00, setting magic numbers, initializing and writing
 the superblock and inodes, etc. Must return a specified success/error code. */
 
-int tfs_mkfs(char *filename, int nBytes);
+int tfs_mkfs(char *filename, int nBytes) {
+   
+   fd = openDisk(filename, nBytes);
+   
+   int i = 0;
+   for(i = 0; i < nBytes / BLOCKSIZE; i++) {
+      cleanBlock(i);
+   }
+   
+   superBlock = calloc(sizeof(SuperBlock), 1);
+   
+   rootINode = makeInode(1, "rootNode", 0);
+   writeBlock(fd, 1, rootINode);
+   
+   RequiredInfo requiredInfo;
+   requiredInfo.type = 1;
+   requiredInfo.magicNumber = 0x45;
+   requiredInfo.blockNumber = 0;
+   
+   superBlock->required = requiredInfo;
+   superBlock->rootInode = rootINode;
+   
+   FreeBlock *freeBlocks = NULL;
+
+      for(i = 2; i <  nBytes / BLOCKSIZE; i++) {
+         FreeBlock *fb = makeFreeBlock(i);
+         if(freeBlocks == NULL) {
+            freeBlocks = fb;
+         }
+         
+         FreeBlock *temp = freeBlocks;         
+         while(temp->next) {
+            temp = temp->next;
+         }
+         temp->next = fb;
+      }
+   
+   superBlock->freeBlocks = freeBlocks;
+}
 
 /* tfs_mount(char *filename) “mounts” a TinyFS file system located within
 ‘filename’. tfs_unmount(void) “unmounts” the currently mounted file system.
