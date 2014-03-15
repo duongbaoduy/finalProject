37ba1781b9323cc02e1475785e18a1e411bfc2ee
mward03 <mwcp@live.com>
2014-03-14 21:30:19 -0700

    temp strcmp

--- tinyFS.c
+++ tinyFS.c
@@ -860,4 +860,18 @@ void tfs_displayFragments() {
 		}
 	}
 	
+int my_strcmp(char *str1, char *str2)
+{
+  int           i;
+
+  i = 0;
+  while (str1[i] || str2[i])
+    {
+      if (str1[i] != str2[i])
+        return (str1[i] - str2[i]);
+      i++;
+    }
+  return (0);
+}
+	
 }
